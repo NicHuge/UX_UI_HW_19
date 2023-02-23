33218229e5af5389c1cde828fe534d36f6c970a5
NicHuge <111213851+NicHuge@users.noreply.github.com>
2023-02-23 12:09:00 -0500

    Hw 19

---
+++ JS/index.js
@@ -0,0 +1,10 @@
+// Smooth Scroll Animation
+$("#scroll-design").click(function () {
+    $("html, body").animate({ scrollTop: $('#scrollitem1').offset().top - 50 }, 'slow');
+});
+
+$("#scroll-cases").click(function () {
+    $("html, body").animate({ scrollTop: $('#scrollitem2').offset().top - 50 }, 'slow');
+});
+//   Scale Animation
+
